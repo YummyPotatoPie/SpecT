e03b0050063b206572830e53c6d4c47e6124a165
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-08-03 08:26:47 +0300

    release

--- SpecT.py
+++ SpecT.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 
 from telethon.sync import TelegramClient
 from telethon import connection
@@ -19,13 +20,24 @@ client.start()
 
 async def main():
 	"""Main script"""
-	channel = await channel_input()
+	channel, channel_url = await channel_input()
 	participants = await dump_all_participants(channel)
+	write_csv_data(channel_url[13:len(channel_url)], participants)
 
-	# Temporary code
-	for i in range(len(participants)):
-		await client.send_message(channel, participants[i].stringify())
-	# Temporary code
+
+def get_participant_name(participant):
+	name = ""
+	if (participant.first_name != None):
+		name += participant.first_name
+	if (participant.last_name != None):
+		name += " " + participant.last_name
+	return name
+
+
+def get_participant_phone(participant):
+	if participant.phone == None:
+		return "None"
+	return participant.phone
 
 
 async def channel_input():
@@ -37,7 +49,7 @@ async def channel_input():
 		except Exception:
 			print("\nInvalid url, please try again.")
 			continue
-		return channel
+		return (channel, channel_url)
 
 
 async def dump_all_participants(channel):
@@ -49,13 +61,30 @@ async def dump_all_participants(channel):
 	filter_user = ChannelParticipantsSearch('')
 
 	while True:
-		participants = await client(GetParticipantsRequest(channel, filter_user, offset_user, limit_user, hash=0))
-		if not participants.users:
-			break
-		all_participants.extend(participants.users)
-		offset_user += len(participants.users)
+		try:
+			participants = await client(GetParticipantsRequest(channel, filter_user, offset_user, limit_user, hash=0))
+			if not participants.users:
+				break
+			all_participants.extend(participants.users)
+			offset_user += len(participants.users)
+		except:
+			print("For this action you need admin permission")
+			break;
 
 	return all_participants
+
+
+def readable_info(participant):
+	return str(participant.id) + "," + str(get_participant_name(participant)) + "," + \
+		str(participant.scam) + "," + str(get_participant_phone(participant))
+
+
+def write_csv_data(csv_file_name, participants):
+	with open(os.path.join("csv_data", csv_file_name + ".csv"), "w", encoding="utf-8") as csv_file:
+		csv_file.write("id, name, is_scam, phone\n")
+		for i in range(len(participants)):
+			csv_file.write(readable_info(participants[i]) + "\n")
+
 
 with client:
 	client.loop.run_until_complete(main())
---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+def write_config(write_mode):
+    with open('config.ini', write_mode) as config:
+        config.write("[Telegram]\n")
+        config.write("api_id = " + str(input("Please input api_id: ") + "\n"))
+        config.write("api_hash = " + str(input("Please input api_hash: ") + "\n"))
+        config.write("username = " + str(input("Please input your username: ")))
+
+try:
+    if os.path.exists('config.ini'):
+        request = input("Rewrite data? Input y (yes) or n (no): ")
+        if request == 'y':
+            write_config("w")
+        else:
+            input("Enter any button to exit: ")
+    else:
+        write_config("x")
+        if os.path.exists('csv_data'):
+            pass
+        else:
+            os.mkdir('csv_data')
+except Exception:
+    print("Something went wrong")
